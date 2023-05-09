27d531d592d3ffd7e602119761555f517a3feca7
Terence Lin <tlin41@dons.usfca.edu>
2023-05-09 03:35:45 -0700

    have progress page.

--- pen_game_admin/src/pages/progress.jsx
+++ pen_game_admin/src/pages/progress.jsx
@@ -3,25 +3,33 @@ import "../style.scss"
 import { useNavigate } from "react-router-dom";
 import io from 'socket.io-client';
 const socket = io.connect("https://evening-ridge-47791.herokuapp.com");
+const rooms = [];
 
 export default function Progress() {
-    const [timeLeft, setTimeLeft] = useState({});
+    const [rooms, setRooms] = useState([]);
 
     useEffect(() => {
         // Listen for the "start game" event from the server
-        socket.on("get_room", (roomId) => {
+        socket.on("get_room", (roomId,timer) => {
             // Initialize the time left for the new room to be 60 seconds
             const room = {
                 roomId: roomId,
-                
+                timeLeft: timer,
             };
+            setRooms([...rooms, room]);
         });
     });
+
 
     return (
         <div>
             <h1>Progress</h1>
-            <h1>Time Left: {timeLeft}</h1>
+            {rooms.map((room) => (
+                <div key={room.roomId}>
+                    <h2>Room ID: {room.roomId}</h2>
+                    <h2>Time Left: {room.timeLeft}</h2>
+                </div>
+            ))}
         </div>
     );
 }
